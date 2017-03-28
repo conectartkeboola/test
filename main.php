8c78feda375aba1081f80091d9a2bc871beffb69
x88354 <x88354@nbhpbo03.ilas.1188.local>
2017-03-28 23:12:43 +0200

    create table dle tutoriálu

--- main.php
+++ main.php
@@ -1,6 +1,12 @@
 <?php
 
 require_once "vendor/autoload.php";
+use Keboola\Csv\CsvFile;
+use Keboola\StorageApi\Client;
+
+$client = new Client([
+    'token' => '432-13435-260a8737d0edd4d9b435b53bc30cb14b0b75cb57',
+]);
 
 // načtení konfiguračního souboru
 $ds         = DIRECTORY_SEPARATOR;
@@ -13,8 +19,10 @@ $dataDir    = getenv("KBC_DATADIR");
 
 $file = "test";
 $csv = new \Keboola\Csv\CsvFile($dataDir."out".$ds."tables".$ds."out_".$file.".csv");
+
+//$csvFile = new CsvFile('./new-table.csv');
    
 $csv->writeRow(["Id", "Name"]);
 $csv->writeRow(["1", "test"]);
     
-$client->createTable("out.c-daktela-reports", "test", $csv);
+$client->createTableAsync("out.c-daktela-reports", "test", $csv);
