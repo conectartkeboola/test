422ce9b305bb44be5f705a64c91e75d16de86898
x88354 <x88354@nbhpbo03.ilas.1188.local>
2017-03-28 17:48:15 +0200

    test.php - test vytvoření tabulky (2)

--- main.php
+++ main.php
@@ -1,9 +1,20 @@
 <?php
-$temp = new Temp();
-    $temp->initRunFolder();
-    $filename = $temp->createFile("table.csv");
-    $csv = new CsvFile($filename->getPathname());
-    $csv->writeRow(["Id", "Name"]);
-    $csv->writeRow(["1", "test"]);
+
+require_once "vendor/autoload.php";
+
+// načtení konfiguračního souboru
+$ds         = DIRECTORY_SEPARATOR;
+$dataDir    = getenv("KBC_DATADIR");
+
+//$temp = new Temp();
+//$temp->initRunFolder();
+//$filename = $temp->createFile("table.csv");
+//$csv = new CsvFile($filename->getPathname());
+
+$file = "test";
+$csv = new \Keboola\Csv\CsvFile($dataDir."out".$ds."tables".$ds."out_".$file.".csv");
+   
+$csv->writeRow(["Id", "Name"]);
+$csv->writeRow(["1", "test"]);
     
 $client->createTable("out.c-daktela-reports", "test", $csv);
